38695b58c838b14941fd8eef051071afef0abba7
2003thanhquy <quythanhhuynh2003@gmail.com>
2025-05-28 16:40:45 +0700

    ...

--- services/config/api.config.ts
+++ services/config/api.config.ts
@@ -1,6 +1,6 @@
 // API Configuration
 export const API_CONFIG = {
-  BASE_URL: 'https://pcs-travesti-notified-possibilities.trycloudflare.com',
+  BASE_URL: 'http://localhost:8009',
   API_VERSION: 'v1',
   TIMEOUT: 10000, // 10 seconds
   HEADERS: {
